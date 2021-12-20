9adfed11da40f99f556734a7d612de9fb3a2c9c5
Victor Nea <vnea@octo.com>
2021-12-20 12:04:49 +0100

    nvim: Remove Save and exist mappings

--- nvim/lua/mapping/general.lua
+++ nvim/lua/mapping/general.lua
@@ -4,11 +4,6 @@ local mapping = require("mapping.module")
 mapping.inoremap("jj", "<Esc>")
 mapping.vnoremap("jj", "<Esc>")
 
--- Save and exit
-mapping.nnoremap("ZZ", ":x<CR>")
-mapping.inoremap("ZZ", "<Esc>:x<CR>")
-mapping.vnoremap("ZZ", "<Esc>:x<CR>")
-
 -- Exit without change
 mapping.inoremap("qq", "<Esc>:q!<CR>")
 mapping.nnoremap("qq", ":q!<CR>")
