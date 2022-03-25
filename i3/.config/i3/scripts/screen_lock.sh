4acc2dcd7dc5aebc0ba2b5810514e4e3ae44a67a
Victor Nea <vnea@octo.com>
2022-03-25 14:06:04 +0100

    i3: More blur when locking

--- i3/.config/i3/scripts/screen_lock.sh
+++ i3/.config/i3/scripts/screen_lock.sh
@@ -28,7 +28,7 @@ i3lock \
 --keyhl-color=$WRONG         \
 --bshl-color=$WRONG          \
 \
---blur 10                    \
+--blur 50                    \
 --clock                      \
 --time-str="%H:%M:%S"        \
 --date-str="%A %d %B"
