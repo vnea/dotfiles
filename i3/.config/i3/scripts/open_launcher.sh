f9f47b27e564b142dcbc302d3582d60a5f92ae93
Victor Nea <victor.nea@octo.com>
2023-02-23 23:38:33 +0100

    rofi: new configuration + choose right theme for rofi launcher

---
+++ i3/.config/i3/scripts/open_launcher.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+# To change theme: https://github.com/adi1090x/rofi#previews (current SHA commit used: 024a948)
+type="type-2"
+style="style-10"
+rofi -show drun -theme ~/.config/rofi/launchers/$type/$style.rasi
