559acf329cb55c3411793d292a46c77f8a8343be
Victor Nea <victor.nea@octo.com>
2023-08-22 17:32:08 +0200

    aliased: add prr

--- aliases/pacman.sh
+++ aliases/pacman.sh
@@ -1,12 +1,6 @@
 #!/usr/bin/env bash
 
 alias pc="pacman"
-
-# Update all packages
 alias pcu="sudo pacman -Syu"
-
-# Remove a package
 alias pcr="sudo pacman -R"
-
-# Search a package with fzf and install it
 alias pcs="pacman -Slq | fzf --multi --preview 'pacman -Si {1}' --preview-window='up:50%' | xargs --no-run-if-empty --open-tty sudo pacman -S"
