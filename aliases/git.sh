a7c8373ac82cdb1d6ea004cc379a7f1c63915f83
Victor Nea <victor.nea@octo.com>
2023-08-22 16:17:58 +0200

    git: add some git aliases

--- aliases/git.sh
+++ aliases/git.sh
@@ -3,27 +3,15 @@
 alias lzg="lazygit"
 alias git="LANG=en_US.UTF-8 git"
 alias glg="git log --graph"
-
-# Undo last commit
 alias gundo="git reset --soft HEAD^"
-
-# Delete outdated branches
 alias gdob="git branch -vv | grep ': gone' | cut -d ' ' -f 3 | xargs git branch -D"
-
-# Remove branches that have already been merged with default branch
 alias gdm="git branch --merged | grep -v '\\*' | xargs -n 1 git branch -d; git remote -v update -p"
-
-# List contributors
 alias gcontributors="git shortlog --summary --numbered --no-merges"
-
-# Copy current branch in clipboard
 alias gcy="git branch --show-current | tr -d '\n' | xclip -sel clip"
-
-# Select branch with fzf
 alias gcob="git branch -a | sed \"s/remotes\/origin\///g\" | sed \"s/\s//g\" | sed \"s/*//\" | sort | uniq | fzf | xargs --no-run-if-empty git checkout"
-
-# Reset current work to current origin branch
 alias grho="git reset --hard origin/\$(git rev-parse --abbrev-ref HEAD)"
+alias glg='git log --graph'
+alias gst='git status --short'
 
 function gist() {
     (&>/dev/null xdg-open "https://gist.github.com/vnea" &)
@@ -33,23 +21,19 @@ function vnea() {
     (&>/dev/null xdg-open "https://github.com/vnea" &)
 }
 
-# Fzf diff
 function zd() {
   preview="git diff $* --color=always -- {-1} | diff-so-fancy"
   git diff "$@" --name-only | fzf -m --ansi --preview-window=right:70% --preview "$preview"
 }
 
-# Fzf diff with main
 function zdm() {
   zd main
 }
 
-# Classic diff with main
 function gdm {
   git diff main
 }
 
-# Squash N last commits
 function gsquash {
   number_of_commits_to_squash=${1}
   if [ -z "$1" ]
