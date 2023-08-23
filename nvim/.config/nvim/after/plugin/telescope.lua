e47255f19a585fb5d7d45753fbbd895f2f856a69
Victor Nea <victor.nea@octo.com>
2023-08-24 01:17:51 +0200

    nvim: update Telescope shortcuts

--- nvim/.config/nvim/after/plugin/telescope.lua
+++ nvim/.config/nvim/after/plugin/telescope.lua
@@ -1,5 +1,5 @@
 local builtin = require("telescope.builtin")
-vim.keymap.set("n", "<Leader>ff", builtin.find_files, { noremap = true, silent = true })
-vim.keymap.set("n", "<Leader>fg", builtin.live_grep, { noremap = true, silent = true })
-vim.keymap.set("n", "<Leader>fb", builtin.buffers, { noremap = true, silent = true })
+vim.keymap.set("n", "<Leader><C-n>", builtin.find_files, { noremap = true, silent = true })
+vim.keymap.set("n", "<C-n>", builtin.git_files, { noremap = true, silent = true })
+vim.keymap.set("n", "<Leader>f", builtin.live_grep, { noremap = true, silent = true })
 
