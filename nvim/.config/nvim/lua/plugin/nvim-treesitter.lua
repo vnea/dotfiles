3f9dc119b2c27e1df8b498d4a9cdfaa1b1262864
Victor Nea <victor.nea@octo.com>
2023-08-23 23:35:27 +0200

    nvim: add configuration for nvim-treesitter

--- nvim/.config/nvim/lua/plugin/nvim-treesitter.lua
+++ nvim/.config/nvim/lua/plugin/nvim-treesitter.lua
@@ -1,5 +1,31 @@
 return {
     "nvim-treesitter/nvim-treesitter",
     tag = "v0.9.1",
-    build = ":TSUpdate"
+    build = ":TSUpdate",
+    config = function () 
+        local configs = require("nvim-treesitter.configs")
+
+        configs.setup({
+            ensure_installed = {
+                "bash",
+                "go",
+                "lua",
+                "markdown",
+                "vim",
+                "vimdoc",
+                "query",
+                "json",
+                "yaml",
+                "javascript",
+                "typescript",
+                "html"
+            },
+            sync_install = false,
+            auto_install = true,
+            highlight = {
+                enable = true,
+                additional_vim_regex_highlighting = false,
+            },
+        })
+    end
 }
