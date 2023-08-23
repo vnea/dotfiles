0ff8682cb655bd462ea844467e72a006affedf75
Victor Nea <victor.nea@octo.com>
2023-08-24 00:34:14 +0200

    nvim: add some options

--- nvim/.config/nvim/lua/config/option.lua
+++ nvim/.config/nvim/lua/config/option.lua
@@ -4,6 +4,13 @@ vim.opt.expandtab = true
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.autowrite = true
+vim.opt.backup = false
+vim.opt.swapfile = false
+vim.opt.smartindent = true
 vim.opt.list = true
+vim.opt.hlsearch = false
+vim.opt.scrolloff = 8
+vim.opt.termguicolors = true
+vim.api.nvim_command("colorscheme desert")
 vim.api.nvim_command("set clipboard+=unnamedplus")
 
