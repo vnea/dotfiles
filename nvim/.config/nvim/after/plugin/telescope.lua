ae326378fa8a9b2f1931fa8269a7eb7ec1d092de
Victor Nea <victor.nea@octo.com>
2023-08-26 15:50:05 +0200

    nvim: make Telescope git_files list untracked files

--- nvim/.config/nvim/after/plugin/telescope.lua
+++ nvim/.config/nvim/after/plugin/telescope.lua
@@ -1,5 +1,4 @@
 local builtin = require("telescope.builtin")
 vim.keymap.set("n", "<Leader><C-n>", builtin.find_files, { noremap = true, silent = true })
-vim.keymap.set("n", "<C-n>", builtin.git_files, { noremap = true, silent = true })
+vim.keymap.set("n", "<C-n>", function() builtin.git_files({ show_untracked = true }) end, { noremap = true, silent = true })
 vim.keymap.set("n", "<Leader>f", builtin.live_grep, { noremap = true, silent = true })
-
