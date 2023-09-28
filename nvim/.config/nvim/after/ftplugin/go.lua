87deac5d5110a103ee727a2275c7da907aa99427
Victor Nea <victor.nea@octo.com>
2023-09-28 22:37:07 +0200

    nvim: add mappings for Go

---
+++ nvim/.config/nvim/after/ftplugin/go.lua
@@ -0,0 +1,3 @@
+vim.keymap.set("n", "<Leader>rt", "<cmd>GoTestFunc<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<Leader>rT", "<cmd>GoTestFile<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<Leader>tu", "<cmd>GoTest<CR>", { noremap = true, silent = true })
