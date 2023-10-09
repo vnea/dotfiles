5a4129a7b38b7f146d86b1ba0f256054daa0a9e0
Victor Nea <victor.nea@octo.com>
2023-10-09 22:56:27 +0200

    nvim: migrate from lsp-zero v2 to v3

--- nvim/.config/nvim/after/plugin/lsp-zero.lua
+++ nvim/.config/nvim/after/plugin/lsp-zero.lua
@@ -1,6 +1,7 @@
-local lsp = require("lsp-zero").preset({})
+local lsp_zero = require("lsp-zero")
+lsp_zero.on_attach(function(_, bufnr)
+    lsp_zero.default_keymaps({buffer = bufnr})
 
-lsp.on_attach(function(_, bufnr)
     -- See :help lsp-zero-keybindings to learn the available actions
     vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, { silent = true, remap = false, buffer = bufnr })
     vim.keymap.set("n", "gs", function() vim.lsp.buf.signature_help() end, { silent = true, remap = false, buffer = bufnr })
@@ -12,18 +13,26 @@ lsp.on_attach(function(_, bufnr)
     vim.keymap.set("n", "<S-F2>", function() vim.diagnostic.goto_prev() end, { silent = true, remap = false, buffer = bufnr })
 end)
 
-require("lspconfig").lua_ls.setup(lsp.nvim_lua_ls())
-
-lsp.setup()
+require("mason").setup({})
+require("mason-lspconfig").setup({
+    handlers = {
+        lsp_zero.default_setup,
+        lua_ls = function()
+            local lua_opts = lsp_zero.nvim_lua_ls()
+            require("lspconfig").lua_ls.setup(lua_opts)
+        end,
+        gopls = function ()
+            require("lspconfig").gopls.setup({
+                cmd_env = {GOFLAGS="-tags=integration"}
+            })
+        end
+    }
+})
 
 local cmp = require("cmp")
-
 cmp.setup({
-    mapping = {
-        -- Confirm completion
-        ["<CR>"] = cmp.mapping.confirm({ select = false }),
-
-        -- Trigger completion menu
+    mapping = cmp.mapping.preset.insert({
+        ["<CR>"] = cmp.mapping.confirm({select = false}),
         ["<C-Space>"] = cmp.mapping.complete(),
-    }
+    })
 })
