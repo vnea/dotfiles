c1e562eaf9b3eabdd72ef6ccf0a87e54e8ecb7c7
Victor Nea <victor.nea@octo.com>
2023-10-12 15:34:30 +0200

    nvim: remove telescope from scretch dependencies

--- nvim/.config/nvim/lua/plugin/scretch-nvim.lua
+++ nvim/.config/nvim/lua/plugin/scretch-nvim.lua
@@ -1,6 +1,5 @@
 return {
     "Sonicfury/scretch.nvim",
-    requires = "nvim-telescope/telescope.nvim",
     config = function()
         require("scretch").setup {
             scretch_dir = vim.fn.stdpath("config") .. "/scretch/",
