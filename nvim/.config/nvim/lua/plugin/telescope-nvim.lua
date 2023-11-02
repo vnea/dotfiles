3f5d9d3bc0adc5284f5a615a8c10cb03c202d233
Victor Nea <victor.nea@octo.com>
2023-11-02 13:41:51 +0100

    nvim: add advanced-git-search.nvim plugin

--- nvim/.config/nvim/lua/plugin/telescope-nvim.lua
+++ nvim/.config/nvim/lua/plugin/telescope-nvim.lua
@@ -7,6 +7,7 @@ return {
         { "nvim-telescope/telescope-ui-select.nvim" },
         { "nvim-telescope/telescope-dap.nvim" },
         { "nvim-telescope/telescope-live-grep-args.nvim", version = "^1.0.0" },
+        { "aaronhallaert/advanced-git-search.nvim" },
     },
     config = function()
         local telescope = require("telescope")
@@ -14,6 +15,7 @@ return {
         telescope.load_extension("ui-select")
         telescope.load_extension("dap")
         telescope.load_extension("live_grep_args")
+        require("telescope").load_extension("advanced_git_search")
         telescope.setup({
             defaults = {
                 file_ignore_patterns = {
@@ -21,6 +23,11 @@ return {
                     ".idea",
                     "node_modules",
                 },
+            },
+            extensions = {
+                advanced_git_search = {
+                    -- See Config
+                }
             },
         })
     end,
