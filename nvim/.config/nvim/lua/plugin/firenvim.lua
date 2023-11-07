440f74ce660985977a7fb3279ee07ef0bba4c62d
Victor Nea <victor.nea@octo.com>
2023-11-07 17:55:00 +0100

    nvim: put all firenvim configuration in one file

--- nvim/.config/nvim/lua/plugin/firenvim.lua
+++ nvim/.config/nvim/lua/plugin/firenvim.lua
@@ -7,4 +7,13 @@ return {
     build = function()
         vim.fn["firenvim#install"](0)
     end,
+    config = function()
+        vim.g.firenvim_config = {
+            localSettings = {
+                [".*"] = {
+                    takeover = "never"
+                }
+            }
+        }
+    end,
 }
