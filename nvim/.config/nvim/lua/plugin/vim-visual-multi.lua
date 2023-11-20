35c516da6431937cdc7aba177fd63402200cc24d
Victor Nea <victor.nea@octo.com>
2023-11-20 20:35:19 +0100

    nvim: add mappings for multi-cursors

--- nvim/.config/nvim/lua/plugin/vim-visual-multi.lua
+++ nvim/.config/nvim/lua/plugin/vim-visual-multi.lua
@@ -3,9 +3,11 @@ return {
     branch = "master",
     init = function()
         vim.g.VM_maps = {
-            ["Find Under"]      = "<A-j>",
-            ["Add Cursor Down"] = "<Leader><Down>",
-            ["Add Cursor Up"]   = "<Leader><Up>",
+            ["Find Under"]         = "<A-j>",
+            ["Find Subword Under"] = "<A-j>",
+            ["Select All"]         = "<A-S-j>",
+            ["Add Cursor Down"]    = "<Leader><Down>",
+            ["Add Cursor Up"]      = "<Leader><Up>",
         }
     end,
 }
