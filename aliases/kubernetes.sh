b142e3be64212b9fe6d76594c996b17203c3c84f
Victor Nea <victor.nea@octo.com>
2024-01-12 11:30:27 +0100

    aliases: add ku/kd for kubernetes

--- aliases/kubernetes.sh
+++ aliases/kubernetes.sh
@@ -4,3 +4,13 @@ alias kc="kubectx"
 
 alias kubectl="kubecolor"
 compdef kubecolor=kubectl
+
+
+function ku {
+    export KUBECONFIG=$(ls -1 ~/.kube/*.conf | awk '{print $NF}' | fzf)
+}
+
+function kd {
+    unset KUBECONFIG
+}
+
