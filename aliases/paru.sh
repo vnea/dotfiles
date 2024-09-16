fae25436ff5e46f6de4e869a4d9f6f31a6e3413d
Victor Nea <victor.nea@gmail.com>
2024-09-16 05:35:12 +0200

    zsh: fix paru with fzf alias

--- aliases/paru.sh
+++ aliases/paru.sh
@@ -3,5 +3,5 @@
 alias pru="paru -Sua"
 alias prc="paru --clean"
 alias prr="paru --remove"
-alias prs="paru --color=always -Sl | sed -e 's: :/:; s/ unknown-version//' | fzf --multi --ansi --preview='paru --color=always -Si {1}' --preview-window='up:50%' | xargs --no-run-if-empty --open-tty paru -S --cleanafter"
+alias prs="paru --color=always -Sl | sed -e 's: :/:; s/unknown-version//' | fzf --multi --ansi --preview='paru --color=always -Si {1}' --preview-window='up:50%' | xargs --no-run-if-empty --open-tty paru -S --cleanafter"
 
