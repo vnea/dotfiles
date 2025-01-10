cde73b9ae61ef406816241635de6a74ebb31ae2b
Victor Nea <>
2025-01-10 18:42:05 +0100

    aliases: remove useless kubernetes aliases

--- aliases/kubernetes.sh
+++ aliases/kubernetes.sh
@@ -5,12 +5,3 @@ alias kc="kubectx"
 alias kubectl="kubecolor"
 compdef kubecolor=kubectl
 
-
-function ku {
-    export KUBECONFIG=$(ls -1 ~/.kube/*.conf | awk '{print $NF}' | fzf)
-}
-
-function kd {
-    unset KUBECONFIG
-}
-
