508cf2e6a1090bd226e33e5a25954a1cf068bc5c
Victor Nea <>
2025-01-12 00:57:15 +0100

    nvim: change mappings for vim visual multi

--- nvim/.config/nvim/lua/plugin/vim-visual-multi.lua
+++ nvim/.config/nvim/lua/plugin/vim-visual-multi.lua
@@ -3,9 +3,9 @@ return {
     branch = "master",
     init = function()
         vim.g.VM_maps = {
-            ["Find Under"]         = "<Leader>j",
-            ["Find Subword Under"] = "<Leader>j",
-            ["Select All"]         = "<Leader>J",
+            ["Find Under"]         = "☁", -- <Cmd-j>, check Alacritty conf
+            ["Find Subword Under"] = "☁", -- <Cmd-j>, check Alacritty conf
+            ["Select All"]         = "☂", -- <Cmd-S-j>, check Alacritty conf
             ["Add Cursor Down"]    = "<Leader><Down>",
             ["Add Cursor Up"]      = "<Leader><Up>",
         }
