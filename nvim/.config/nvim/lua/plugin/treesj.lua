d7d77973efc833d336b4f24c27456304623428ea
Victor Nea <>
2025-01-17 01:32:51 +0100

    nvim: add treesj plugin

---
+++ nvim/.config/nvim/lua/plugin/treesj.lua
@@ -0,0 +1,12 @@
+return {
+  "Wansmer/treesj",
+  keys = {
+      "<space>m",
+      "<space>j",
+      "<space>s",
+  },
+  dependencies = { "nvim-treesitter/nvim-treesitter" },
+  config = function()
+    require("treesj").setup({})
+  end,
+}
