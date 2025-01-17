75c2d2c4528c70955850ffe45568df29e1914f23
Victor Nea <>
2025-01-17 01:16:02 +0100

    nvim: add gitsigns.nvim plugin

---
+++ nvim/.config/nvim/lua/plugin/gitsigns-nvim.lua
@@ -0,0 +1,19 @@
+return {
+    "lewis6991/gitsigns.nvim",
+    config = function()
+        local gitsigns = require("gitsigns")
+        gitsigns.setup({
+            signs_staged_enable = true,
+            current_line_blame = false,
+            current_line_blame_opts = {
+                virt_text = true,
+                virt_text_pos = "eol", -- 'eol' | 'overlay' | 'right_align'
+                delay = 50,
+                ignore_whitespace = false,
+                virt_text_priority = 100,
+                use_focus = true,
+            },
+        })
+        vim.keymap.set("n", "<Leader>gb", gitsigns.toggle_current_line_blame)
+    end,
+}
