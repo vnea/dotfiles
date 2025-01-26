a9d1f616f5cecdba57512509b941daa7de0312e4
Victor Nea <>
2025-01-26 23:17:45 +0100

    nvim: add peek.nvim plugin

---
+++ nvim/.config/nvim/lua/plugin/peek-nvim.lua
@@ -0,0 +1,23 @@
+return {
+    "toppair/peek.nvim",
+    event = { "VeryLazy" },
+    build = "deno task --quiet build:fast",
+    config = function()
+        local peek = require("peek")
+        peek.setup({
+            app = "webview",
+            close_on_bdelete = true,
+            auto_load = true,
+            syntax = false,
+            update_on_change = true,
+
+        })
+        vim.keymap.set("n", "<Leader>mp", function()
+            if peek.is_open() then
+                peek.close()
+            else
+                peek.open()
+            end
+        end)
+    end,
+}
