aadc959334be2bfedefc278b6e615f9e7f707925
Victor Nea <>
2025-01-26 03:45:43 +0100

    nvim: remove color in spell suggestions

--- nvim/.config/nvim/lua/plugin/midnight-nvim.lua
+++ nvim/.config/nvim/lua/plugin/midnight-nvim.lua
@@ -3,6 +3,20 @@ return {
     lazy = false,
     priority = 1000,
     config = function()
+        require("midnight").setup({
+            SpellBad = {
+                style = "underline",
+            },
+            SpellCap = {
+                style = "underline",
+            },
+            SpellLocal = {
+                style = "underline",
+            },
+            SpellRare = {
+                style = "underline",
+            },
+        })
         vim.cmd.colorscheme "midnight"
     end,
 }
