c74fc58e2ab82cfbdb86bc3cfadcc790ca4168b4
Victor Nea <>
2025-02-09 21:18:21 +0100

    nvim: add plugin nvim-spider

---
+++ nvim/.config/nvim/lua/plugin/nvim-spider.lua
@@ -0,0 +1,23 @@
+return {
+    "chrisgrieser/nvim-spider",
+    config = function()
+        vim.keymap.set(
+            { "n", "o", "x" },
+            "w",
+            function() require("spider").motion("w") end,
+            { desc = "Spider-w" }
+        )
+        vim.keymap.set(
+            { "n", "o", "x" },
+            "e",
+            function() require("spider").motion("e") end,
+            { desc = "Spider-e" }
+        )
+        vim.keymap.set(
+            { "n", "o", "x" },
+            "b",
+            function() require("spider").motion("b") end,
+            { desc = "Spider-b" }
+        )
+    end,
+}
