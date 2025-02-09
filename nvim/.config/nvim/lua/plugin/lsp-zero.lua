255364f46c8c2a596d0262de194e499536c7e290
Victor Nea <>
2025-02-09 22:41:32 +0100

    nvim: add sign icons for lsp

--- nvim/.config/nvim/lua/plugin/lsp-zero.lua
+++ nvim/.config/nvim/lua/plugin/lsp-zero.lua
@@ -83,6 +83,17 @@ return {
                 end,
             })
 
+            vim.diagnostic.config({
+                signs = {
+                    text = {
+                        [vim.diagnostic.severity.ERROR] = "✘",
+                        [vim.diagnostic.severity.WARN] = "▲",
+                        [vim.diagnostic.severity.HINT] = "⚑",
+                        [vim.diagnostic.severity.INFO] = "»",
+                    },
+                },
+            })
+
             require("mason-lspconfig").setup({
                 ensure_installed = {
                     "ansiblels",
