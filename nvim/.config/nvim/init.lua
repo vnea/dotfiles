e92654a84e342a9f1ce1c147b89fc88c81b3d686
Victor Nea <>
2025-02-09 19:50:17 +0100

    nvim: update config for nvim-tree

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,7 +1,3 @@
--- nvim-tree configuration: https://github.com/nvim-tree/nvim-tree.lua#quick-start
-vim.g.loaded_netrw = 1
-vim.g.loaded_netrwPlugin = 1
-
 -- Must be first because we define leader mapping here and Lazy needs it to be defined first
 require("config.mapping")
 
--- nvim/.config/nvim/lua/plugin/nvim-tree.lua
+++ nvim/.config/nvim/lua/plugin/nvim-tree.lua
@@ -4,7 +4,18 @@ return {
     lazy = false,
     dependencies = { "nvim-tree/nvim-web-devicons" },
     config = function()
+        vim.g.loaded_netrw = 1
+        vim.g.loaded_netrwPlugin = 1
+
         require("nvim-tree").setup({
+            actions = {
+                open_file = {
+                    resize_window = false,
+                },
+            },
+            diagnostics = {
+                enable = true,
+            },
             filters = {
                 git_ignored = false,
                 custom = {
@@ -12,6 +23,41 @@ return {
                     ".idea",
                 },
             },
+            hijack_directories = {
+                enable = false,
+            },
+            view = {
+                side = "right",
+            },
         })
+
+        -- Source: https://github.com/nvim-tree/nvim-tree.lua/wiki/Recipes#remember-nvimtree-window-size
+        local remember_window_size = function()
+            local view = require("nvim-tree.view")
+            local api = require("nvim-tree.api")
+            local augroup = vim.api.nvim_create_augroup
+            local autocmd = vim.api.nvim_create_autocmd
+
+            -- save nvim-tree window width on WinResized event
+            augroup("save_nvim_tree_width", { clear = true })
+            autocmd("WinResized", {
+                group = "save_nvim_tree_width",
+                pattern = "*",
+                callback = function()
+                    local filetree_winnr = view.get_winnr()
+                    if filetree_winnr ~= nil and vim.tbl_contains(vim.v.event["windows"], filetree_winnr) then
+                        vim.t["filetree_width"] = vim.api.nvim_win_get_width(filetree_winnr)
+                    end
+                end,
+            })
+
+            -- restore window size when openning nvim-tree
+            api.events.subscribe(api.events.Event.TreeOpen, function()
+                if vim.t["filetree_width"] ~= nil then
+                    view.resize(vim.t["filetree_width"])
+                end
+            end)
+        end
+        remember_window_size()
     end,
 }
