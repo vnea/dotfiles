923cfeb6632113882d93a15a07c840fa6917aa9e
Victor Nea <>
2025-02-11 22:04:23 +0100

    nvim: change colors for better diff

--- nvim/.config/nvim/lua/plugin/theme.lua
+++ nvim/.config/nvim/lua/plugin/theme.lua
@@ -3,6 +3,25 @@ return {
     name = "catppuccin",
     priority = 1000,
     config = function()
+        require("catppuccin").setup({
+            custom_highlights = function(colors)
+                return {
+                    DiffAdd = { bg = "#15633b" },
+                    DiffChange = { bg = "#15633b" },
+                    DiffText = { bg = "#199c59" },
+                    DiffDelete = {
+                        bg = "#3F0001",
+                        fg = "#520001",
+                    },
+                    Folded = { bg = "#2e2e47" },
+                    UfoFoldedEllipsis = {
+                        fg = colors.lavender,
+                        bg = "#2e2e47",
+                    },
+                }
+            end
+        })
+        vim.opt.fillchars:append("diff:╱")
         vim.cmd("colorscheme catppuccin-mocha")
     end,
 }
