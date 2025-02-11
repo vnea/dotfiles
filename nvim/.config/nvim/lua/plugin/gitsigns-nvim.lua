f67ff3a65d8a9764f999e6a3dc18566b84fc090e
Victor Nea <>
2025-02-12 00:17:16 +0100

    nvim: add some mappings got gitsigns.nvim

--- nvim/.config/nvim/lua/plugin/gitsigns-nvim.lua
+++ nvim/.config/nvim/lua/plugin/gitsigns-nvim.lua
@@ -1,20 +1,57 @@
 return {
     "lewis6991/gitsigns.nvim",
     config = function()
-        local gitsigns = require("gitsigns")
-        gitsigns.setup({
+        require("gitsigns").setup({
             signs_staged_enable = true,
             current_line_blame = false,
             current_line_blame_opts = {
                 virt_text = true,
-                virt_text_pos = "eol", -- 'eol' | 'overlay' | 'right_align'
+                virt_text_pos = "eol", -- "eol" | "overlay" | "right_align"
                 delay = 50,
                 ignore_whitespace = false,
                 virt_text_priority = 100,
                 use_focus = true,
             },
+            on_attach = function(bufnr)
+                local gitsigns = require("gitsigns")
+
+                local function map(mode, l, r, opts)
+                    opts = opts or {}
+                    opts.buffer = bufnr
+                    vim.keymap.set(mode, l, r, opts)
+                end
+
+                -- Navigation
+                map("n", "<Leader>hn", function()
+                    if vim.wo.diff then
+                        vim.cmd.normal({ "]c", bang = true })
+                    else
+                        gitsigns.nav_hunk("next")
+                    end
+                end)
+
+                map("n", "<Leader>hN", function()
+                    if vim.wo.diff then
+                        vim.cmd.normal({ "[c", bang = true })
+                    else
+                        gitsigns.nav_hunk("prev")
+                    end
+                end)
+
+                -- Actions
+                map("n", "<Leader>gb", gitsigns.blame)
+                map("n", "<leader>td", gitsigns.toggle_linehl)
+                map("n", "<leader>hp", gitsigns.preview_hunk)
+
+                map("n", "<leader>hs", gitsigns.stage_hunk)
+                map("v", "<leader>hs", function() gitsigns.stage_hunk({ vim.fn.line("."), vim.fn.line("v") }) end)
+
+                map("n", "<leader>hr", gitsigns.reset_hunk)
+                map("v", "<leader>hr", function() gitsigns.reset_hunk({ vim.fn.line("."), vim.fn.line("v") }) end)
+
+                map("n", "<leader>hd", gitsigns.diffthis)
+                map("n", "<leader>hD", function() gitsigns.diffthis("~") end)
+            end,
         })
-        vim.keymap.set("n", "<Leader>gb", gitsigns.blame)
-        vim.keymap.set("n", "<leader>grh", gitsigns.reset_hunk)
     end,
 }
