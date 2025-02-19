451d4a21743537171ad8168528a70b6c8077c592
Victor Nea <>
2025-02-19 12:58:48 +0100

    aliases: add kgetall

--- aliases/kubernetes.sh
+++ aliases/kubernetes.sh
@@ -6,3 +6,8 @@ alias ks="k9s"
 alias kubectl="kubecolor"
 compdef kubecolor=kubectl
 
+# Source: https://github.com/luksa/kubectl-plugins/blob/master/kubectl-really-get-all
+function kgetall() {
+    RESOURCES=$(kubectl api-resources --verbs=list --namespaced -o name | tr "\n" ,)
+    kubectl get ${RESOURCES%?} --show-kind --ignore-not-found $@
+}
