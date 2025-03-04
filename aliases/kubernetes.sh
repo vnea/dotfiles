6ad113c69f68296953f6f2f7a0f83ff2b1c6794c
Victor Nea <>
2025-03-04 15:34:53 +0100

    aliased: add some functions to select k8s namespace with cache

--- aliases/kubernetes.sh
+++ aliases/kubernetes.sh
@@ -11,3 +11,16 @@ function kgetall() {
     RESOURCES=$(kubectl api-resources --verbs=list --namespaced -o name | tr "\n" ,)
     kubectl get ${RESOURCES%?} --show-kind --ignore-not-found $@
 }
+
+# Source: https://github.com/ahmetb/kubectx/issues/313
+function kns() {
+  if namespace=$(< "$HOME/.local/share/kubectx-cache/$(kubectx -c)" fzf); then
+      kubectl config set-context --current --namespace="$namespace";
+  fi
+}
+
+# Source: https://github.com/ahmetb/kubectx/issues/313
+function kns-update-cache() {
+  mkdir -p "$HOME/.local/share/kubectx-cache"
+  kubectl get ns -o json | jq -r '.items[].metadata.name' > "$HOME/.local/share/kubectx-cache/$(kubectx -c)"
+}
