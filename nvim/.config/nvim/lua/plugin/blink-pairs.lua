34fd64cd1d1e958c97f3755d0cc48a7ce02b3b31
Victor Nea <>
2025-03-23 03:12:24 +0100

    nvim: replace nvim-autopairs by blink.pairs

---
+++ nvim/.config/nvim/lua/plugin/blink-pairs.lua
@@ -0,0 +1,40 @@
+return {
+    "saghen/blink.pairs",
+    version = '*', -- (recommended) only required with prebuilt binaries
+
+    dependencies = "saghen/blink.download",
+
+    --- @module 'blink.pairs'
+    --- @type blink.pairs.Config
+    opts = {
+        mappings = {
+            enabled = true,
+            pairs = {
+                ['('] = ')',
+                ['['] = ']',
+                ['{'] = '}',
+                ["'"] = { closing = "'", enter = false },
+                ['"'] = { closing = '"', enter = false },
+                ['`'] = { closing = '`', enter = false },
+            },
+        },
+        highlights = {
+            enabled = true,
+            -- Update colors in "nvim/.config/nvim/lua/plugin/theme.lua"
+            groups = {
+                "BlinkPairsOne",
+                "BlinkPairsTwo",
+                "BlinkPairsThree",
+                "BlinkPairsFour",
+                "BlinkPairsFive",
+                "BlinkPairsSix",
+                "BlinkPairsSeven",
+                "BlinkPairsEight",
+                "BlinkPairsNine",
+            },
+            priority = 200,
+            ns = vim.api.nvim_create_namespace("blink.pairs"),
+        },
+        debug = false,
+    }
+}
--- nvim/.config/nvim/lua/plugin/theme.lua
+++ nvim/.config/nvim/lua/plugin/theme.lua
@@ -10,11 +10,23 @@ return {
                     DiffChange = { bg = "#013601" },
                     DiffText = { bg = "#017d40" },
                     DiffDelete = { bg = "#3F0001" },
+
                     Folded = { bg = "#2e2e47" },
                     UfoFoldedEllipsis = {
                         fg = colors.lavender,
                         bg = "#2e2e47",
                     },
+
+                    -- Colors come from here: https://plugins.jetbrains.com/plugin/10080-rainbow-brackets
+                    BlinkPairsOne = { fg = "#fdd370" },
+                    BlinkPairsTwo = { fg = "#c4e993" },
+                    BlinkPairsThree = { fg = "#8fdcff" },
+                    BlinkPairsFour = { fg = "#c892e7" },
+                    BlinkPairsFive = { fg = "#f18d71" },
+                    BlinkPairsSix = { fg = "#a4bd29" },
+                    BlinkPairsSeven = { fg = "#0060a0" },
+                    BlinkPairsEight = { fg = "#d17424" },
+                    BlinkPairsNine = { fg = "#f9c26c" },
                 }
             end
         })
