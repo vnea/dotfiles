878dee09f0891629600a103a7c7093a3ee192478
Victor Nea <>
2025-03-23 23:20:30 +0100

    alacritty: change toggle_theme.sh script to make it better

--- alacritty/.config/alacritty/scripts/toggle_theme.sh
+++ alacritty/.config/alacritty/scripts/toggle_theme.sh
@@ -1,19 +1,24 @@
 #!/usr/bin/env bash
 
-readonly alacritty_config_file=~/dotfiles/alacritty/.config/alacritty/alacritty.toml
-
-readonly theme_dark=github_dark_default.toml
-readonly theme_light=github_light_default.toml
-
-toggleTheme() {
-    readonly from_theme="${1}"
-    readonly to_theme="${2}"
-    sd "${from_theme}" "${to_theme}" "${alacritty_config_file}"
+setupTheme() {
+    readonly current_theme_file="${1}"
+    readonly new_theme_file="${2}"
+    cat "$new_theme_file" > "$current_theme_file"
 }
 
-if rg --fixed-strings --quiet "${theme_dark}" "${alacritty_config_file}"; then
-    toggleTheme "${theme_dark}" "${theme_light}"
-else
-    toggleTheme "${theme_light}" "${theme_dark}"
+readonly theme_file="$HOME/dotfiles/alacritty/.config/alacritty/themes/theme.toml"
+readonly theme_dark_file="$HOME/dotfiles/alacritty/.config/alacritty/themes/github_dark_default.toml"
+readonly theme_light_file="$HOME/dotfiles/alacritty/.config/alacritty/themes/github_light_default.toml"
+
+if [ ! -f "$theme_file" ]; then
+    setupTheme "$theme_file" "$theme_dark_file"
+    exit 0
 fi
 
+if rg --fixed-strings --quiet "# Theme: dark" "${theme_file}"; then
+    new_theme_file="$theme_light_file"
+else
+    new_theme_file="$theme_dark_file"
+fi
+
+setupTheme "$theme_file" "$new_theme_file"
