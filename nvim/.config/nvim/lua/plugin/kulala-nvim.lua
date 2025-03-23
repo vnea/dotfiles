411430ebca2ffaef2ec5f81d64f7b1b3270ba52c
Victor Nea <>
2025-03-23 23:54:42 +0100

    nvim: add exmaple for kulala.nvim plugin

--- nvim/.config/nvim/lua/plugin/kulala-nvim.lua
+++ nvim/.config/nvim/lua/plugin/kulala-nvim.lua
@@ -4,3 +4,34 @@ return {
         global_keymaps = true,
     },
 }
+
+--[[ Examples
+###
+
+POST https://dummy.restapiexample.com/api/v1/create
+content-type: application/json
+
+{
+  "name": "test",
+  "salary": "123",
+  "age": "23"
+}
+
+###
+
+POST https://dummy.restapiexample.com/api/v1/create
+content-type: application/json
+
+< new-dummy.json
+
+###
+
+GET https://dummy.restapiexample.com/api/v1/employees
+accept: application/json
+
+###
+
+GET https://google.com
+
+###
+]]
