ee0beca6c19846fbb44ed0619152dae9934b5a87
Victor Nea <>
2025-03-29 13:19:48 +0100

    nvim: change settings for mini-files plugin

--- nvim/.config/nvim/lua/plugin/mini-files.lua
+++ nvim/.config/nvim/lua/plugin/mini-files.lua
@@ -4,7 +4,12 @@ return {
     config = function()
         require("mini.files").setup({
             options = {
+                permanent_delete = false,
                 use_as_default_explorer = false,
+            },
+            mappings = {
+                go_in  = "<Tab>",
+                go_out = "<S-Tab>",
             },
         })
 
