bd87c4d4c86315c2b7e6d4cd7303fdfcbe784210
Victor Nea <>
2025-03-30 14:10:31 +0200

    aliases: add ability to pass jira issue in j

--- aliases/jira.sh
+++ aliases/jira.sh
@@ -4,7 +4,8 @@
 source ~/dotfiles/aliases/utils/open.sh
 
 function j() {
-    jira_issue_id="$(git branch --show-current | rg -o '(^\w+-\d+)')"
+    local jira_issue_id=$1
+    [[ -z $jira_issue_id ]] && jira_issue_id="$(git branch --show-current | rg --only-matching '(^\w+-\d+)')"
 
     if [[ -n $jira_issue_id ]]; then
         [[ -z $ALIASES_JIRA_URL ]] && { echo 'The environment variable "ALIASES_JIRA_URL" must be defined.'; return 1; }
