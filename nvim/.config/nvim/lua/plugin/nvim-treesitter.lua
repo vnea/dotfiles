2df484e77cb2ed5aebfcf0db04972e0c352080a1
Victor Nea <>
2025-06-10 15:51:28 +0200

    nvim: update plugins

--- nvim/.config/nvim/lua/plugin/nvim-treesitter.lua
+++ nvim/.config/nvim/lua/plugin/nvim-treesitter.lua
@@ -1,6 +1,7 @@
 return {
     "nvim-treesitter/nvim-treesitter",
     dependencies = { "nvim-treesitter/nvim-treesitter-textobjects" },
+    branch = "master",
     build = ":TSUpdate",
     config = function()
         require("nvim-treesitter.configs").setup({
