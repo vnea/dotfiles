ed9a35bf455fc7f4cf61e96689dc6270e4cc75c5
Victor Nea <>
2025-07-24 13:07:51 +0200

    nvim: add api/front/cronjob in dial

--- nvim/.config/nvim/lua/plugin/dial-nvim.lua
+++ nvim/.config/nvim/lua/plugin/dial-nvim.lua
@@ -43,6 +43,7 @@ return {
                 augend.constant.new { elements = { "stable", "test" } },
                 augend.constant.new { elements = { "DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL" } },
                 augend.constant.new { elements = { "debug", "info", "warning", "error", "critical" } },
+                augend.constant.new { elements = { "api", "front", "cronjob" } },
             },
         }
 
