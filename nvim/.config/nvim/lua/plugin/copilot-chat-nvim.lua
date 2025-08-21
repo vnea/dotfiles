79f7b9bdd11a43ee721bd0121b41d2fe91b17f27
Victor Nea <>
2025-08-21 09:42:07 +0200

    nvim: set claude-sonnet-4 as model in CopilotChat plugin

--- nvim/.config/nvim/lua/plugin/copilot-chat-nvim.lua
+++ nvim/.config/nvim/lua/plugin/copilot-chat-nvim.lua
@@ -3,6 +3,7 @@ return {
     cmd = { "CopilotChatToggle", "CopilotChatPrompts" },
     build = "make tiktoken",
     opts = {
+        model = "claude-sonnet-4",
         mappings = {
             complete = {
                 insert = "", -- Trick to be able to use <Tab> (auto completion from Copilot) in the prompt chat
