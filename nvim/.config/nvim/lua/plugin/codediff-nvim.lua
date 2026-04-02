26612a14128d191659f9dae19e12812db41dba40
Victor Nea <>
2026-04-02 14:01:40 +0200

    nvim: change conflict result position to center in codediff.nvim

--- nvim/.config/nvim/lua/plugin/codediff-nvim.lua
+++ nvim/.config/nvim/lua/plugin/codediff-nvim.lua
@@ -2,4 +2,9 @@ return {
     "esmuellert/codediff.nvim",
     dependencies = { "MunifTanjim/nui.nvim" },
     cmd = "CodeDiff",
+    opts = {
+        diff = {
+            conflict_result_position = "center",
+        },
+    }
 }
