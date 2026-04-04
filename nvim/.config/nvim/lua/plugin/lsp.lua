85f406ae5a5ab26b4113c13122b83b5165d97340
Victor Nea <>
2026-04-04 20:32:25 +0200

    nvim: install yamlls LSP

--- nvim/.config/nvim/lua/plugin/lsp.lua
+++ nvim/.config/nvim/lua/plugin/lsp.lua
@@ -87,6 +87,7 @@ return {
                     "lua_ls",
                     "terraformls",
                     "vtsls",
+                    "yamlls",
                 },
             })
 
@@ -107,6 +108,21 @@ return {
                     json = {
                         schemas = require("schemastore").json.schemas(),
                         validate = { enable = true },
+                    },
+                },
+            })
+
+            vim.lsp.config("yamlls", {
+                settings = {
+                    yaml = {
+                        schemaStore = {
+                            -- You must disable built-in schemaStore support if you want to use
+                            -- this plugin and its advanced options like `ignore`.
+                            enable = false,
+                            -- Avoid TypeError: Cannot read properties of undefined (reading 'length')
+                            url = "",
+                        },
+                        schemas = require("schemastore").yaml.schemas(),
                     },
                 },
             })
